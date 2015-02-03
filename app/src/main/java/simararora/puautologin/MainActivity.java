9a1c51b030fae55dcf4f03498c99ae6ce48b65f5
Simar <simarpreetsingharora@gmail.com>
2015-02-03 22:50:17 +0530

    Logout Task Created

---
+++ app/src/main/java/simararora/puautologin/MainActivity.java
@@ -0,0 +1,21 @@
+package simararora.puautologin;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LoginTask(MainActivity.this).execute();
+            }
+        });
+    }
+}
